b82f863ab3c29b4383c093f7dc0a9da2484e12db
Colin Spear <colinspear@gmail.com>
2021-09-05 09:34:35 -0700

    start streamlit app

--- src/streamlit-dash.py
+++ src/streamlit-dash.py
@@ -12,14 +12,23 @@ df = pd.read_csv(project_path / 'data/2013-2021-clean.csv')
 df['FOLDERYEAR'] = (df['FOLDERYEAR'] + 2000).astype('str')
 
 
-st.title('Vancouver Business Churn')
+st.title('Vancouver Business License Dashboard')
 
+# count businesses by license status
 status_count = df.groupby(['FOLDERYEAR', 'new_status'])['id'].count().unstack()
 status_count = status_count.fillna(0)
 status_count = status_count.astype('int')
 status_count.columns = [s.lower() for s in status_count.columns]
 
-active = status_count.loc[status_count.index==max(status_count.index), 'issued']
-st.write(active.values)
+# number of currently active businesses
+active = status_count.iloc[-1]['issued']
+st.write(active)
+
+# number of new businesses this year
+max_year = max(set(df['FOLDERYEAR']))
+y = set(df.loc[df['FOLDERYEAR'] == max_year, 'id'].unique())
+y_1 = set(df.loc[df['FOLDERYEAR'] == str(int(max_year)-1), 'id'].unique())
+st.write(len(y - y_1))
+
 
 st.line_chart(status_count)
